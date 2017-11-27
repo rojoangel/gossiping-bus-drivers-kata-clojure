5a1cfbe39ff06f9d737bcd560f3125c8727db69d
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-11-28 00:32:21 +0100

    save some code created using rdd

--- src/gossiping_bus_drivers/core.clj
+++ src/gossiping_bus_drivers/core.clj
@@ -1,1 +1,13 @@
 (ns gossiping-bus-drivers.core)
+
+(defn xxx []
+  (filter
+    (fn [[_ stops]]
+      (not= (count (set stops)) (count stops)))             ; all-stops-different?
+    (map-indexed                                            ; min->stops
+      vector
+      (take 480                                             ; day routes
+            (map vector
+                 (cycle [2 2 3])
+                 (cycle [2 3 1])
+                 (cycle [5 5 6]))))))
