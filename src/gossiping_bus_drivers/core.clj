423f43265fc261022d9be6377bab72c771318a4b
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-12-03 18:10:56 +0100

    extract 'day-schedule-gossips' and 'mins-to-get-all-gossips-around' helper functions

--- src/gossiping_bus_drivers/core.clj
+++ src/gossiping_bus_drivers/core.clj
@@ -19,14 +19,21 @@
   (let [gossips-exchanged (gossips-exchanged gossips stops)]
     (for [stop stops] (get gossips-exchanged stop))))
 
-(defn mins-to-get-all-gossips-around [drivers-stops]
-  (let [day-schedule (day-schedule drivers-stops)
-        drivers-count (count drivers-stops)
+(defn- day-schedule-gossips [drivers-stops]
+  (let [drivers-count (count drivers-stops)
         gossips (gossips drivers-count)
-        mins-to-get-all-gossips-around (count
-                                         (take-while (fn [drivers-gossips]
-                                                       (not-every? #(= drivers-count (count %)) drivers-gossips))
-                                                     (reductions exchange-gossips gossips day-schedule)))]
+        day-schedule (day-schedule drivers-stops)]
+    (reductions exchange-gossips gossips day-schedule)))
+
+(defn- all-drivers-aware-of-all-gossips? [drivers-gossips]
+  (let [drivers-count (count drivers-gossips)]
+    (every? #(= drivers-count (count %)) drivers-gossips)))
+
+(defn mins-to-get-all-gossips-around [drivers-stops]
+  (let [mins-to-get-all-gossips-around
+        (count
+          (take-while (complement all-drivers-aware-of-all-gossips?)
+                      (day-schedule-gossips drivers-stops)))]
     (if (> mins-to-get-all-gossips-around 480)
       :never
       mins-to-get-all-gossips-around)))
