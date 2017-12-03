f002d3f972c0d12284af4daf3d34e02f9d1e787a
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-12-03 10:57:05 +0100

    test route

--- src/gossiping_bus_drivers/core.clj
+++ src/gossiping_bus_drivers/core.clj
@@ -1,13 +1,26 @@
 (ns gossiping-bus-drivers.core)
 
-(defn xxx []
-  (filter
-    (fn [[_ stops]]
-      (not= (count (set stops)) (count stops)))             ; all-stops-different?
-    (map-indexed                                            ; min->stops
-      vector
-      (take 480                                             ; day routes
-            (map vector
-                 (cycle [2 2 3])
-                 (cycle [2 3 1])
-                 (cycle [5 5 6]))))))
+(defn route [driver-stops]
+  (take 480 (cycle driver-stops)))
+
+(defn routes [all-drivers-stops]
+  (apply map vector (map route all-drivers-stops)))
+
+(defn gossips [drivers-count]
+  (for [gossip (range 1 (inc drivers-count))]
+    (conj #{} (keyword (str gossip)))))
+
+; some sort of reduce?
+;gossips  ;meets/stops  ;gossips
+; [#{:1}    [2   ->       [ #{:1 :2}
+;  #{:2}     2   ->         #{:1 :2}
+;  #{:3}     3   ->         #{:3 :4}
+;  #{:4}]    3]  ->         #{:3 :4}]
+; don't use true/false 'cos more than one pair of drivers can meet at a stop
+; mapping gossips on stops may be helpful {2 #{:1 :2} 3 #{:3 :4}}
+; then it can be added to the set with index = stop (key in the map)
+
+; (reduce (fn [driver-gossip meet]
+;          (let [exchanged-gossips]
+;            add-exchanged-gossips-to-matches)
+;          driver-gossips meets)
--- test/gossiping_bus_drivers/core_test.clj
+++ test/gossiping_bus_drivers/core_test.clj
@@ -2,6 +2,9 @@
   (:require [clojure.test :refer :all]
             [gossiping-bus-drivers.core :refer :all]))
 
-(deftest a-test1
-  (testing "Basic tests"
-    ))
+(deftest a-route-should
+  (testing "have 480 stops"
+    (is (= 480 (count (route [3 1 2 3])))))
+  (testing "routes are cyclic"
+    (let [stops [3 1 2 3]]
+      (is (= [3 1 2 3] (take (count stops) (drop (count stops) (route stops))))))))
